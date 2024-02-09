da1be4b4014df8c84920267ab3dcba0b1d1c91c8
MasterCatPL <107404623+MasterCatPL@users.noreply.github.com>
2024-02-09 21:22:56 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,16 @@
 #!/bin/ash
 rm -rf /home/container/tmp/*
 
+echo "  
+  __  __ _______ _    _ _    _  ____   _____ _______ 
+ |  \/  |__   __| |  | | |  | |/ __ \ / ____|__   __|
+ | \  / |  | |  | |__| | |__| | |  | | (___    | |   
+ | |\/| |  | |  |  __  |  __  | |  | |\___ \   | |   
+ | |  | |  | |  | |  | | |  | | |__| |____) |  | |   
+ |_|  |_|  |_|  |_|  |_|_|  |_|\____/|_____/   |_|   
+                                                     
+                                                     "
+
 echo "⟳ Starting PHP-FPM..."
 /usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
